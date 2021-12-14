5007616983bd370fee01dc826412c24e957f1da6
Sandor Resko <reskosandor@gmail.com>
2021-12-14 14:05:44 +0100

    mesh3d output fixed

--- mesh_3d.py
+++ mesh_3d.py
@@ -29,29 +29,21 @@ def create_grid_3d(dimensions, m):
         print("m must not be greater than the dimension of the mesh")
         exit()
     Z = nx.grid_graph(dims, periodic=False)
-    ##print(nx.info(Z))
-    #nx.draw(Z)
-    #plt.show()
     #---initial_set_(2, m)_begins)---
     #---creating_C---
-    #print("nodes of Z")
-    #print(list(Z.nodes))
     if (m == 1):
         C = Z.copy()
         for x in range(dim1):
             for y in range(dim2):
                 for z in range(dim3):
-                    #print(str(x) + " " + str(y) + " " + str(z))
                     if z > 0:
                         C.remove_node((x, y, z))
-        #print(list(C.nodes))
 
     if (m == 2):
         C = Z.copy()
         for x in range(dim1):
             for y in range(dim2):
                 for z in range(dim3):
-                    #print(str(x) + " " + str(y) + " " + str(z))
                     if y > 0 or z > 0:
                         C.remove_node((x, y, z))
 
@@ -59,13 +51,8 @@ def create_grid_3d(dimensions, m):
         C = nx.Graph()
         C.add_node((0, 0, 0))
 
-    #print(nx.info(C))
-    #print(list(C.nodes))
-    #nx.draw(C)
-    #plt.show()
 
     #---computing shortest paths in C---
-    print(list(C.nodes))
     P = []
     agents = {}
     previous_agents = {}
@@ -76,6 +63,7 @@ def create_grid_3d(dimensions, m):
     #print(color)
     #start with a set of |C| agents in v_(0,0)
     nr_of_agents = C.number_of_nodes()
+    print("color is " + str(color))
 
     # constructing P in a new way
     # v = (0, 0, 0) is in the bottom left corner
@@ -84,9 +72,7 @@ def create_grid_3d(dimensions, m):
         #print(nx.shortest_path(C, source = (0, 0, 0), target = i))
         #P.append(nx.shortest_path(C, source = (0, 0, 0), target = i))
         sublist = []
-        print("(x,y,z) is:")
         (x, y, z) = i
-        print((x, y, z))
 
         sublist.append((0, 0, 0))
         k = 0
@@ -100,15 +86,13 @@ def create_grid_3d(dimensions, m):
                 l = l + 1
                 sublist.append((x, l, z))
 
-        print(sublist)
         P.append(sublist)
-    print("P is :")
-    print(P)
+
     for i in range(nr_of_agents):
         agents[i] = (0, 0, 0)
-    #print(agents)
+    print("agents are " + str(agents))
     functions.color_sync(Z, agents, previous_agents, color, m)
-    #print(color)
+    print("color is " + str(color))
 
     flipped_agents = {}
     #print(len(agents.keys()))
@@ -117,7 +101,7 @@ def create_grid_3d(dimensions, m):
     values = list(agents.values())
     iterations = 0
     # while exists v in C containing more than one agent
-
+    print("Starting INITIAL-SET")
     while len(values) != len(set(values)):
         sum_p = 0
 
@@ -130,79 +114,42 @@ def create_grid_3d(dimensions, m):
                 flipped_agents[value] = [key]
             else:
                 flipped_agents[value].append(key)
-        #print("agents")
-        #print(agents)
-        #print("flipped_agents")
-        #print(flipped_agents)
         v = (0, 0, 0)
         biggest_v = -1
-        print("flipped_agents")
-        print(flipped_agents)
         for key in flipped_agents:
             value = flipped_agents[key]
             if len(value) > 1 and len(value) > biggest_v:
                 v = key
                 biggest_v = len(value)
 
-        print("v is:")
-        print(v)
         truest_list_of_agents_on_v = []
         for key in agents:
             if agents[key] == v:
                 truest_list_of_agents_on_v.append(key)
-        print("truest")
-        print(truest_list_of_agents_on_v)
-        print(len(truest_list_of_agents_on_v))
         # let (v,w_i),...,(v,w_r) be the edges from v included in a path in P
         edges_of_v_in_P = []
         for i in P:
-            #print("i is:")
-            #print(i)
-            #print("len(i) is: ")
-            #print(len(i))
             for j in i:
-                #print("j is:")
-                #print(j)
-                #print("len(j) is:")
-                #print(len(j))
                 if j == v and j != i[-1]:
-                    #print("(i.index(j) is")
-                    #print((i.index(j)))
-                    #print("i[i.index(j)] is")
-                    #print(i[i.index(j)])
                     if len(i) > 1:
                         edge = []
                         edge.append(v)
-                        #print("i indices")
-                        #print(i)
-                        #print(len(i))
-                        #print(i[i.index(j)])
                         edge.append(i[i.index(j)+1])
                         edges_of_v_in_P.append(edge)
-        #print("edges of v in P:")
-        #print(edges_of_v_in_P)
-        #print(len(edges_of_v_in_P))
-        #remove duplicate elements for edges_of_v_in_P
         no_duplicates = []
         for i in edges_of_v_in_P:
             if i not in no_duplicates:
                 no_duplicates.append(i)
-        print("edges_of_v_in_P")
         edges_of_v_in_P = no_duplicates
-        print(edges_of_v_in_P)
 
-        #print(edges_of_v_in_P)
         # let p_i be the number of paths in P containing (v,w_i)
         p = []
         for i in range(len(edges_of_v_in_P)):
             counter = 0
             for j in range(len(P)):
                 if str(edges_of_v_in_P[i]).strip("[]") in str(P[j]):
-                    #print(str(edges_of_v_in_P[i]))
                     counter = counter + 1
             p.append(counter)
-        print("p is:")
-        print(p)
         #checking how many agents do we want to send overall
 
         for i in range(len(p)):
@@ -211,11 +158,6 @@ def create_grid_3d(dimensions, m):
         # for i = 1 to r
         previous_agents = agents.copy()
         for i in range(len(edges_of_v_in_P)):
-            #print(agents)
-            print("i is:")
-            print(i)
-            #print("p[i] is")
-            #print(p[i])
             # move p_i agents to w_i
             true_list_of_agents_on_v = []
             for key in agents:
@@ -228,68 +170,35 @@ def create_grid_3d(dimensions, m):
                 if agents[key] == v and len(list_of_agents_on_v) < p[i]:
                     list_of_agents_on_v.append(key)
             nr_of_agents_on_v = len(list_of_agents_on_v)
-            print("list of agents on v")
-            print(list_of_agents_on_v)
-            print("v is")
-            print(v)
+
 
 
             position_of_agents_on_v = []
 
             # in this for loop, for each k, we only move one agent at a time, k in range(nr_of_agents_on_v total agents
             for k in range(nr_of_agents_on_v):
-                #print("k is:")
-                #print(type(k))
-                #print(k)
-                #print("nr_of_agents_on_v")
-                #print(type(nr_of_agents_on_v))
-                #print(nr_of_agents_on_v)
-                #print("list_of_agents_on_v")
-                #print(type(list_of_agents_on_v))
-                #print(list_of_agents_on_v)
-                #print("edges of v in P")
-                #print(type(edges_of_v_in_P[i]))
-                #print(edges_of_v_in_P[i])
                 position_of_agents_on_v.append(edges_of_v_in_P[i][1])
-                #print("#print(list_of_agents_on_v[k+1])")
-                #print(list_of_agents_on_v[k])
-                #print("position_of_agents_on_v[k]")
-                #print("position_of_agents_on_v[k]")
                 for key in agents:
                     if list_of_agents_on_v[k] == key and len(true_list_of_agents_on_v) > 1:
                         agents[key] = position_of_agents_on_v[k]
                         move_counter = move_counter + 1
-                        #print("agents")
-                        #print(agents)
-
-            #print("moved p_i agents to w_i, agents are right now at:")
-            #print(agents)
-            #print(color)
+            print("agents are " + str(agents))
+            print("color is " + str(color))
         functions.color_sync(Z, agents, previous_agents, color, m)
-        print(agents)
-        print(color)
-        print("iteration is over")
+
         iterations = iterations + 1
-        print("nr of iterations so far")
-        print(iterations)
 
         #print(flipped_agents)
         values = list(agents.values())
-        print("values")
-        print(values)
-        print("set(values)")
-        print(set(values))
         if previous_values == values:
             print("loop stuck")
             exit()
-        print(agents)
         # check for something wrong?
         if sum_p + 1 != len(truest_list_of_agents_on_v):
-            print(sum_p)
-            print(len(true_list_of_agents_on_v))
             print("something horrible happened")
             exit()
     after_init = move_counter
+    print("Starting MESH")
     # algorithm MESH(3, m)
     # constructing canonical path
     if m == 3:
@@ -318,22 +227,18 @@ def create_grid_3d(dimensions, m):
                     else:
                         for j in range(dim2 - 1, -1, -1):
                             canonical_path.append((i, j, k))
-        #print("canonical_path")
-        #print(canonical_path)
-        #print(len(canonical_path))
         # moving agent through canonical path
         for i in range(len(canonical_path) - 1):
             previous_agents = agents.copy()
             agents[0] = canonical_path[i + 1]
             move_counter = move_counter + 1
-            #print(agents[0])
+            print("the agent is " + str(agents[0]))
             functions.color_sync(Z, agents, previous_agents, color, m)
-            #print(color)
+            print("color is " + str(color))
     if m == 2:
         previous_agents = agents.copy()
         functions.color_sync(Z, agents, previous_agents, color, m)
-        print(color)
-        print(agents)
+
         forward_j = False
         for i in (range(dim3)):
             forward_j = not forward_j
@@ -349,9 +254,9 @@ def create_grid_3d(dimensions, m):
                     else:
                         agents[k] = (x, y - 1, z)
                         move_counter = move_counter + 1
-                print(agents)
+                print("agents are " + str(agents))
                 functions.color_sync(Z, agents, previous_agents, color, m)
-                print(color)
+                print("color is " + str(color))
             previous_agents = agents.copy()
             #now all agents moved one in the y coord
             # move one agent at a time, increasing z coord
@@ -361,43 +266,35 @@ def create_grid_3d(dimensions, m):
                     (x, y, z) = agents[k]
                     agents[k] = (x, y, z + 1)
                     move_counter = move_counter + 1
-                print(agents)
+                print("agents are " + str(agents))
                 functions.color_sync(Z, agents, previous_agents, color, m)
-                print(color)
+                print("color is " + str(color))
     if m == 1:
         #print(agents)
         for i in range(dim3-1):
             previous_agents = agents.copy()
             for j in range(nr_of_agents):
 
-                #print("agents: ")
-                #print(type(agents))
-                #print(agents)
+
                 (x, y, z) = agents[j]
                 agents[j] = (x, y, z+1)
                 move_counter = move_counter + 1
-            #print(agents)
+            print("agents are " + str(agents))
             functions.color_sync(Z, agents, previous_agents, color, m)
-            #print(color)
-    #print(list(Z.nodes))
-    #print(color)
-    #print(list(Z.nodes))
-    #print(color)
+            print("color is " + str(color))
+
     nr_of_black_nodes = 0
     for key in color:
         if color[key] == "black":
-            #print(key)
             nr_of_black_nodes = nr_of_black_nodes + 1
-    print(nr_of_black_nodes)
-    #print("nr of iterations")
-    #print(iterations)
+
     for key in color:
         if color[key] == "grey":
             print("some nodes are grey, algorithm failed")
             exit()
     print("no grey nodes remain")
-    print("after_init is " + str(after_init))
-    print("move_counter is " + str(move_counter))
+    print("moves after INITIAL-SET " + str(after_init))
+    print("total moves " + str(move_counter))
     move_counted = move_counter
     move_counter = 0
     end_time = time.time() - start_time
